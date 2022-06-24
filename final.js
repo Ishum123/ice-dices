9ec23a20f123825974c9c0be8debdfb4856b44fb
Yashasweni <94554260+Ishum123@users.noreply.github.com>
2022-06-24 11:53:48 +0530

    Add files via upload

---
+++ final.js
@@ -0,0 +1,24 @@
+
+//alert("Working");
+var temp1= Math.floor(Math.random()*6)+1;
+var temp2= Math.floor(Math.random()*6)+1;
+
+var diceimage1="images/" +"dice" + temp1+ ".png"; 
+var diceimage2= "images/" +"dice" +temp2+".png";
+
+document.querySelector(".img1").setAttribute("src", diceimage1);
+document.querySelector(".img2").setAttribute("src", diceimage2);
+
+if(temp1>temp2){
+ document.querySelector("h1").innerHTML="<em>Player one wins!</em>";
+}
+
+if(temp1<temp2){
+ document.querySelector("h1").innerHTML="<em>Player two wins!</em>";
+}
+   
+if(temp1===temp2){
+ document.querySelector("h1").innerHTML="<em>Woah! It's a tie!!!</em>";
+}
+   
+   
